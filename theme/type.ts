75058dbf7eefea2a867897b5a07f6503cfe2b47a
andreasbbusk <buskmikkelsen@icloud.com>
2026-04-16 10:24:41 +0200

    feat: move theme folder top level

---
+++ theme/type.ts
@@ -0,0 +1,22 @@
+export const text = {
+  family: {
+    heading: "PlusJakartaSans_700Bold",
+    body: "PlusJakartaSans_500Medium",
+    bodyStrong: "PlusJakartaSans_600SemiBold",
+    label: "PlusJakartaSans_600SemiBold",
+  },
+  size: {
+    caption: 12,
+    body: 16,
+    label: 13,
+    title: 28,
+    hero: 36,
+  },
+  lineHeight: {
+    caption: 16,
+    body: 25,
+    label: 18,
+    title: 34,
+    hero: 42,
+  },
+};
