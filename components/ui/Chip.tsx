9049423d89e7765781bc79fa291d48f36fab66dc
andreaskadhede <andreaskadhede@icloud.com>
2026-04-17 10:13:25 +0200

    feat: add Chip component for selectable UI elements

---
+++ components/ui/Chip.tsx
@@ -0,0 +1,60 @@
+import { baseColors } from '@/theme/colors';
+import { space } from '@/theme/space';
+import { text as textTheme } from '@/theme/type';
+import { Pressable, StyleSheet, Text } from 'react-native';
+
+interface ChipProps {
+  label: string;
+  color: string;
+  isSelected?: boolean;
+  setIsSelected?: (selected: boolean) => void;
+}
+
+export default function Chip({
+  label,
+  color,
+  isSelected,
+  setIsSelected,
+}: ChipProps) {
+  return (
+    <Pressable
+      onPress={() => setIsSelected && setIsSelected(!isSelected)}
+      style={[
+        isSelected ? styles.chipSelected : styles.chip,
+        { backgroundColor: isSelected ? color : 'rgb(255, 255, 255, 0.5)' },
+      ]}
+    >
+      <Text style={isSelected ? styles.selectedLabel : styles.label}>
+        {label}
+      </Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  chip: {
+    paddingHorizontal: space.lg,
+    paddingVertical: space.xs,
+    borderRadius: 16,
+    alignSelf: 'flex-start',
+    borderColor: 'rgb(107, 101, 96, 0.19)',
+    borderWidth: 1,
+  },
+  chipSelected: {
+    paddingHorizontal: space.lg,
+    paddingVertical: space.xs,
+    borderRadius: 16,
+    alignSelf: 'flex-start',
+    borderWidth: 1,
+  },
+  label: {
+    color: baseColors.textSoft,
+    fontSize: textTheme.size.sm,
+    fontFamily: textTheme.family.semiBold,
+  },
+  selectedLabel: {
+    color: baseColors.bg,
+    fontSize: textTheme.size.sm,
+    fontFamily: textTheme.family.semiBold,
+  },
+});
